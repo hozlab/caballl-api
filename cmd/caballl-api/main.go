5b7e3f46291ccb49d86e96cfb59e3b7ac1fec92e
ryoksmt <ryo.arima.ksmt@gmail.com>
2020-10-31 03:31:08 +0900

    add echo

--- cmd/caballl-api/main.go
+++ cmd/caballl-api/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
-
+import (
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"net/http"
+)
 func main() {
-	fmt.Println("vim-go")
+	// Echoのインスタンス作る
+	e := echo.New()
+
+	// 全てのリクエストで差し込みたいミドルウェア（ログとか）はここ
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// ルーティング
+	e.GET("/hello", MainPage())
+
+	// サーバー起動
+	e.Start(":1323")
+}
+
+func MainPage() echo.HandlerFunc {
+	return func(c echo.Context) error {     //c をいじって Request, Responseを色々する
+		return c.String(http.StatusOK, "Hello World")
+	}
 }
